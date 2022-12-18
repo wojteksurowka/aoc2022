3ad31fb3355bf894f48135e3e044f2a1e07cddb6
Wojtek Surowka <wojtek.surowka@thectgrid.com>
2022-12-18 17:42:22 +0000

    Day 17 part 2

--- src/day17.erl
+++ src/day17.erl
@@ -1,5 +1,5 @@
 -module(day17).
--export([part1/0]).
+-export([part1/0, part2/0]).
 
 input() ->
     hd(aoc_input:read(?MODULE, "^(.+)$", [binary])).
@@ -78,10 +78,41 @@ do_shape(ShapeName, JetIndex, Jet, Well) ->
     {XYShape, UpdatedJetIndex} = move({3, getmax(Well) + 4, shape(ShapeName)}, JetIndex, Jet, Well),
     {add(XYShape, Well), UpdatedJetIndex}.
 
-part1() ->
+max_steps(Steps) ->
     Jet = input(),
     Shapes = {line, cross, invl, vline, square},
     ShapesLen = tuple_size(Shapes),
     getmax(element(1, lists:foldl(fun (Index, {Well, JetIndex}) ->
         do_shape(element(Index rem ShapesLen + 1, Shapes), JetIndex, Jet, Well)
-    end, {new_well(), 0}, lists:seq(0, 2022 - 1)))).
+    end, {new_well(), 0}, lists:seq(0, Steps - 1)))).
+
+part1() ->
+    max_steps(2022).
+
+part2() ->
+    Jet = input(),
+    Shapes = {line, cross, invl, vline, square},
+    ShapesLen = tuple_size(Shapes),
+    {_, _, DiffMax, Index, DiffIndex} = hd(lists:reverse(lists:keysort(1, maps:values(element(3, lists:foldl(fun (Index, {Well, JetIndex, Map}) ->
+        {UpdatedWell, UpdatedJetIndex} = do_shape(element(Index rem ShapesLen + 1, Shapes), JetIndex, Jet, Well),
+        Key = {Index rem ShapesLen + 1, UpdatedJetIndex},
+        UpdatedMap = case Map of
+            #{Key := {1, PreviousMax, _, PreviousIndex, _}} ->
+                Map#{Key := {2, getmax(UpdatedWell), getmax(UpdatedWell) - PreviousMax, Index, Index - PreviousIndex}};
+            #{Key := {Count, PreviousMax, DiffMax, PreviousIndex, IndexDiff}} ->
+                case getmax(UpdatedWell) of
+                    Max when Max == PreviousMax + DiffMax ->
+                        case PreviousIndex + IndexDiff == Index of
+                            true -> Map#{Key := {Count + 1, PreviousMax + DiffMax, DiffMax, Index, IndexDiff}};
+                            false -> Map#{Key := {1, Max, 0, Index, 0}}
+                        end;
+                    Max ->
+                        Map#{Key := {1, Max, 0, Index, 0}}
+                end;
+            _ ->
+                Map#{Key => {1, getmax(UpdatedWell), 0, Index, 0}}
+        end,
+        {UpdatedWell, UpdatedJetIndex, UpdatedMap}
+    end, {new_well(), 0, #{}}, lists:seq(0, 100000))))))),
+    Steps = Index + (1000000000000 - Index) rem DiffIndex,
+    max_steps(Steps) + ((1000000000000 - Index) div DiffIndex) * DiffMax.
